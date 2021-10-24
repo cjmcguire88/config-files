7e9b2d220e11e01e46a1e1493ddedfc8ad0b60cf
cjmcguire88 <cjmcguire88@gmail.com>
2021-10-24 15:53:42 -0400

    10/24/21

--- rofi/powermenu/powermenu.sh
+++ rofi/powermenu/powermenu.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-## Author  : Aditya Shakya
-## Mail    : adi1090x@gmail.com
-## Github  : @adi1090x
-## Twitter : @adi1090x
-
 dir="$HOME/.config/rofi/powermenu"
 rofi_command="rofi -theme $dir/powermenu.rasi"
 
